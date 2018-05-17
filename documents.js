b1bb3b8f7defc4e21035357df64520ac193826a7
Hayk <hayk.pilos@gmail.com>
2018-05-17 15:48:39 +0400

    fix: Make general cleanup
    Change-Id: Ie2c0dd662b4f0c315c30cf4e3bcfbb355dd2c894

--- documents.js
+++ documents.js
@@ -1,3 +1,17 @@
+// Copyright 2018 Hayk Pilosyan <hayk.pilos@gmail.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License. You may
+// obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 const invoice = require('ubl/src/xa/ubl/invoice');
 const GoogleStorage = require('./google-storage');
 const LocalTempStorage = require('./local-temp-storage');
@@ -77,8 +91,6 @@ class Documents {
     if (this.locations[k]) {
       return fn(this.locations[k]);
     }
-
-    console.log(`! invalid storage location (key=${k})`);
   }
 }
 
